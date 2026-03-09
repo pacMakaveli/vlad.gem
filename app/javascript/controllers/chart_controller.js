fdc65184d464100f8d9bc9d72ef4e50eefede28c
pacMakaveli <pacMakaveli90@gmail.com>
2026-03-09 14:06:15 +0000

    Use default export for Chart.js import

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { Chart } from "chart.js/auto"
+import Chart from "chart.js/auto"
 
 // Connects to data-controller="chart"
 export default class extends Controller {
